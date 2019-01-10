60dd70351aa550ea326de8b617117f662f2d02db
Darderion <Eobard1SA@gmail.com>
2019-01-10 18:05:57 +0300

    Added UnitTests class

---
+++ ORA/Tests/UnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORA.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Map_AddSubtitles_DictCount()
+        {
+            Map map = (new Map("Test name 1", "Test video URL 1", 10, 20))
+                .AddSubtitle(3, "Test 3 (1)")
+                .AddSubtitle(5, "Test 5 (2)")
+                .AddSubtitle(8, "Test 8 (3)");
+            Assert.AreEqual(3, map.dict.Count);
+        }
+
+        [TestMethod]
+        public void Map_AddSubtitles_SubtitlesCount()
+        {
+            Map map = (new Map("Test name 1", "Test video URL 1", 10, 20))
+                .AddSubtitle(3, "Test 3 (1)")
+                .AddSubtitle(5, "Test 5 (2)")
+                .AddSubtitle(8, "Test 8 (3)");
+            Assert.AreEqual(3, map.subtitles.Count);
+        }
+    }
+}
